0fe4166f189fb2a7750f8adf022369509228499b
Ubuntu <ubuntu@ip-172-31-23-200.eu-west-3.compute.internal>
2023-09-01 21:55:38 +0000

    rework majeur

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -1,3 +1,12 @@
+
+locals {
+  azs = data.aws_availability_zones.available.names
+}
+
+data "aws_availability_zones" "available" {
+  exclude_names =["eu-west-3c"]
+}
+
 resource "aws_vpc" "vpc" {
     cidr_block =  var.vpc_cidr
     enable_dns_support   = true
@@ -35,4 +44,54 @@ resource "aws_default_route_table" "private_rt" {
   tags = {
     Name = "private_rt"
   }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_subnet" "public_subnet" {
+  count  = length(local.azs)
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = var.public_cidrs[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = local.azs[count.index]
+
+  tags = {
+    Name = "public-${count.index + 1}"
+  }
+}
+
+# because private subnet is associeted to default route table wich is private
+resource "aws_route_table_association" "public_assoc" {
+  count = length(local.azs)
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Always use ressource is prefer than inline declaration
+resource "aws_security_group" "sg" {
+  name        = "public_sg"
+  description = "Security group for public instances"
+  vpc_id      = aws_vpc.vpc.id
+}
+
+resource "aws_security_group_rule" "ingress_all" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "-1"
+  cidr_blocks       = [var.access_ip, var.jenkins_ip]
+  security_group_id = aws_security_group.sg.id
+}
+
+resource "aws_security_group_rule" "egress_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sg.id
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,19 @@
-variable vpc_cidr {
+variable "vpc_cidr" {
   type        = string
   default     = "10.20.0.0/16"
+}
+
+variable "public_cidrs" {
+  type = list(string)
+  default = ["10.20.1.0/24", "10.20.2.0/24"]
+}
+
+variable "access_ip" {
+  type    = string
+  default = "86.206.242.52/32"
+}
+
+variable "jenkins_ip" {
+  type    = string
+  default = "15.188.105.8/32"
 }
